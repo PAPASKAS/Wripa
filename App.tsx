7aa471098289d2dcd5c00d1b226339d3ad554f56
Pavel <Papaskaaaa@gmail.com>
2022-04-28 21:12:58 +0300

    for save eject

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const screenWidth = window.screen.width;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app!AAA</Text>
+      {screenWidth}
       <StatusBar style="auto" />
     </View>
   );
